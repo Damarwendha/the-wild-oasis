428d8fdfd5f2a981e44d317e3219dd35766360bb
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-11-12 19:00:41 +0700

    🗑 Cleaning Useless Code

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,55 +1,37 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const NavList = styled.ul`
+const StyledUl = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 0.8rem;
 `;
 
 const StyledNavLink = styled(NavLink)`
-  &:link,
-  &:visited {
-    display: flex;
-    align-items: center;
-    gap: 1.2rem;
-
-    color: var(--color-grey-600);
-    font-size: 1.6rem;
-    font-weight: 500;
-    padding: 1.2rem 2.4rem;
-    transition: all 0.3s;
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.6rem;
+  font-weight: 500;
+  padding: 1.2rem 2.4rem;
+  width: 100%;
+  height: 100%;
+  color: var(--color-grey-400);
+  transition: all 0.3s;
 
   /* This works because react-router places the active class on the active NavLink */
   &:hover,
   &:active,
-  &.active:link,
-  &.active:visited {
-    color: var(--color-grey-800);
-    background-color: var(--color-grey-50);
-    border-radius: var(--border-radius-sm);
-  }
-
-  & svg {
-    width: 2.4rem;
-    height: 2.4rem;
-    color: var(--color-grey-400);
-    transition: all 0.3s;
-  }
-
-  &:hover svg,
-  &:active svg,
-  &.active:link svg,
-  &.active:visited svg {
+  &.active,
+  &.active {
     color: var(--color-brand-600);
+    background-color: var(--color-grey-100);
   }
 `;
 
 function MainNav() {
   return (
     <nav>
-      <NavList>
+      <StyledUl>
         <li>
           <StyledNavLink to="dashboard">
             <span>Home</span>
@@ -75,7 +57,7 @@ function MainNav() {
             <span>Settings</span>
           </StyledNavLink>
         </li>
-      </NavList>
+      </StyledUl>
     </nav>
   );
 }
