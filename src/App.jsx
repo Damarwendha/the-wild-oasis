5bfb32db1450d21b5b5641f2c28dc2adbda10f4a
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-11-13 13:18:55 +0700

    🪓 Setup and Install React Query Devtools

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Dashboard from "@/pages/Dashboard";
 import Account from "@/pages/Account";
 import Bookings from "@/pages/Bookings";
@@ -65,6 +66,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
