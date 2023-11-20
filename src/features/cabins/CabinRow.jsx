e5f5be8d915ff8b54189521ae0b722f424f4e18e
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-11-20 12:04:49 +0700

    🏡 refactor(cabins): reuse table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,24 +3,13 @@ import { HiTrash } from "react-icons/hi2";
 import { BiDuplicate, BiEdit } from "react-icons/bi";
 
 import { formatCurrency } from "@/utils/helpers";
-
-import Modal from "@/ui/Modal";
-import ConfirmDelete from "@/ui/ConfirmDelete";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin";
+
+import Modal from "@/ui/Modal";
+import Table from "@/ui/Table";
+import ConfirmDelete from "@/ui/ConfirmDelete";
 import CreateCabinForm from "./CreateEditCabinForm";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -61,7 +50,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row>
         <Img src={image} alt="Cabin" />
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity}</div>
@@ -98,7 +87,7 @@ function CabinRow({ cabin }) {
             <BiDuplicate />
           </button>
         </div>
-      </TableRow>
+      </Table.Row>
     </>
   );
 }
